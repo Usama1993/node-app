29dab377389be2d4c8e76f171ce0cda73b8d3172
Usama Naseer <usaman1993@gmail.com>
2016-03-31 01:59:54 +0500

    Updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http=require('http');
   fs = require('fs');
+  var port=Number(process.env.PORT || 3000);
   
   fs.readFile('./Task.html', function (err, html) {
     if (err) {
@@ -9,5 +10,5 @@ var http=require('http');
 http.createServer(function(request,response){
 	response.writeHeader(200,{'Content-Type':'text/html'});
 	response.write(html);
-response.end();}).listen(8000);});
+response.end();}).listen(port);});
 
